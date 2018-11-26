a8dc4e54fb1d854c6dd93f64d5c22ae061f48782
Dante Fernandez <durant.crimson@icloud.com>
2018-11-26 13:56:48 -0700

    adjust main

--- iMii_v6/iMii_v6/__init__.py
+++ iMii_v6/iMii_v6/__init__.py
@@ -8,5 +8,8 @@ def main(global_config, **settings):
         config.include('pyramid_chameleon')
         config.include('.routes')
         config.add_route('splash', '/splash')
+        config.add_static_view( 'static', 'static', cache_max_age=3600 )
+        config.add_static_view( 'graphics', 'static/graphics' )
+
         config.scan()
     return config.make_wsgi_app()
