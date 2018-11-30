27352bdeca169263d1ab4d6f09c02ac1ab341130
Dante Fernandez <durant.crimson@icloud.com>
2018-11-30 15:36:06 -0700

    preparing for go-live

--- iMii_v6/iMii_v6/views/default.py
+++ iMii_v6/iMii_v6/views/default.py
@@ -1,8 +1,8 @@
 from pyramid.view import view_config
 
 
-# @view_config(route_name='home', renderer='../templates/mytemplate.pt')
-@view_config(route_name='home', renderer='../templates/splash.pt')
+@view_config(route_name='home', renderer='../templates/iMii.pt')
+# @view_config(route_name='home', renderer='../templates/splash.pt')
 def my_view(request):
     return {'project': 'iMii_v6'}
 
