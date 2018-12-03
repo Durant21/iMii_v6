0833260d09218f80500bf0ba5c206c2a431cff45
Dante Fernandez <durant.crimson@icloud.com>
2018-12-03 13:30:30 -0700

    refactoring file load

--- iMii_v6/iMii_v6/common/logfile.py
+++ iMii_v6/iMii_v6/common/logfile.py
@@ -1,7 +1,7 @@
 def w (sString) :
-    f = open( "/Users/dantefernandez/Projects/iMii/iMii_v6/iMii_v6/iMii_v6/logs/status.log", "a" )
+    # f = open( "/Users/dantefernandez/Projects/iMii/iMii_v6/iMii_v6/iMii_v6/logs/status.log", "a" )
 
-    # f = open("/webapps/iMii_v6/iMii_v6/iMii_v6/logs/status.log","a")
+    f = open("/webapps/iMii_v6/iMii_v6/iMii_v6/iMii_v6/logs/status.log","a")
 
     f.write( sString + "\n" )
 
