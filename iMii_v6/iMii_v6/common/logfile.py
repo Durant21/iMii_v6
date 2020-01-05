604c0cb80219dd6e7f0d2ff6489c8bd2de46e1bd
dante <durant.crimson@icloud.com>
2020-01-05 15:18:48 -0700

    added new teacher

--- iMii_v6/iMii_v6/common/logfile.py
+++ iMii_v6/iMii_v6/common/logfile.py
@@ -1,5 +1,5 @@
 def w (sString) :
-    f = open( "/Users/dantefernandez/Projects/iMii/iMii_v6_source/iMii_v6/iMii_v6/iMii_v6/logs/status.log","a")
+    f = open( "/home/dante/Projects/iMii/v6/iMii_v6/iMii_v6/iMii_v6/logs/status.log","a")
     # f = open("/webapps/iMii_v6/iMii_v6/iMii_v6/iMii_v6/logs/status.log","a")
 
     f.write( sString + "\n" )
